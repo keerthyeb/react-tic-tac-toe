e9ad2a5863ef823d9784088218f7800dacc84a1b
Keerthyb <keerthyb@IN-Keerthyb.local>
2019-04-02 17:45:41 +0530

    Updates

---
+++ precache-manifest.33f6eda51cd1b21e8a931fcf6a15ce5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "679dc4b7dd7cef3a524d",
+    "url": "/react-tic-tac-toe/static/css/main.b659c21d.chunk.css"
+  },
+  {
+    "revision": "679dc4b7dd7cef3a524d",
+    "url": "/react-tic-tac-toe/static/js/main.cb32a8f0.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/react-tic-tac-toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "5b82d17dc1aefff21249169ccf33f250",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
